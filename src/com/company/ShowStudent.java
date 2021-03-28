748fe47041da46f927f7893e53af942cc58987b5
Christian S <chris.salapang@gmail.com>
2021-03-28 20:04:13 +0800

    added priv to some attributes and edited some output

--- src/com/company/ShowStudent.java
+++ src/com/company/ShowStudent.java
@@ -6,9 +6,9 @@ public class ShowStudent {
     public static void main(String[] args) {
         Student s1 = new Student();
 
-        System.out.println("Student 1 ID: " + s1.getId());
-        System.out.println("Student 1 Credits : " + s1.getCredit());
-        System.out.println("Student 1 Points: "+s1.getPointsEarned());
-        System.out.println("THE GPA IS: " + s1.gpa);
+        System.out.println("Student 1 ID is: " + s1.getId());
+        System.out.println("Student 1 Credits is: " + s1.getCredit());
+        System.out.println("Student 1 Points is: "+s1.getPointsEarned());
+        System.out.println("THE GPA is: " + s1.gpa);
     }
 }
